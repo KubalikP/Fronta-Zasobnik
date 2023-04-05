0ef6b3e9b3a6d0367880b3f726075cb39af1af5e
KubalikP <104193829+KubalikP@users.noreply.github.com>
2023-04-05 08:47:43 +0200

    Update Zasobnik.java

--- src/Zasobnik.java
+++ src/Zasobnik.java
@@ -1,2 +1,57 @@
 public class Zasobnik<T> {
+    private Node<T> head;
+    private int size;
+
+    // Constructor
+    public LIFO() {
+        this.head = null;
+        this.size = 0;
+    }
+
+    // Node class
+    private static class Node<T> {
+        private T data;
+        private Node<T> next;
+
+        public Node(T data, Node<T> next) {
+            this.data = data;
+            this.next = next;
+        }
+    }
+
+    // Push a new item onto the top of the stack
+    public void push(T item) {
+        Node<T> newNode = new Node<>(item, head);
+        head = newNode;
+        size++;
+    }
+
+    // Pop the top item off the stack and return it
+    public T pop() {
+        if (isEmpty()) {
+            throw new RuntimeException("Stack is empty");
+        }
+        T item = head.data;
+        head = head.next;
+        size--;
+        return item;
+    }
+
+    // Peek at the top item on the stack without removing it
+    public T peek() {
+        if (isEmpty()) {
+            throw new RuntimeException("Stack is empty");
+        }
+        return head.data;
+    }
+
+    // Check if the stack is empty
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    // Get the number of items in the stack
+    public int size() {
+        return size;
+    }
 }
