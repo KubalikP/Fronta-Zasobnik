626053b2f5b7164d6c9006a67bec9fdfd63c70c7
KubalikP <104193829+KubalikP@users.noreply.github.com>
2023-04-05 08:52:06 +0200

    Update Fronta.java

--- src/Fronta.java
+++ src/Fronta.java
@@ -1,2 +1,69 @@
-public class Fronta {
+public class Fronta<T> {
+    private T value;
+    private Node<T> next;
+
+    public Fronta(T value) {
+        this.value = value;
+        this.next = null;
+    }
+
+    public T getValue() {
+        return value;
+    }
+
+    public void setValue(T value) {
+        this.value = value;
+    }
+
+    public Node<T> getNext() {
+        return next;
+    }
+
+    public void setNext(Node<T> next) {
+        this.next = next;
+    }
+}
+
+public class FIFOQueue<T> {
+    private Node<T> head;
+    private Node<T> tail;
+
+    public FIFOQueue() {
+        head = null;
+        tail = null;
+    }
+
+    public boolean isEmpty() {
+        return head == null;
+    }
+
+    public void enqueue(T value) {
+        Node<T> node = new Node<>(value);
+        if (isEmpty()) {
+            head = node;
+            tail = node;
+        } else {
+            tail.setNext(node);
+            tail = node;
+        }
+    }
+
+    public T dequeue() {
+        if (isEmpty()) {
+            throw new NoSuchElementException();
+        }
+        T value = head.getValue();
+        head = head.getNext();
+        if (head == null) {
+            tail = null;
+        }
+        return value;
+    }
+
+    public T peek() {
+        if (isEmpty()) {
+            throw new NoSuchElementException();
+        }
+        return head.getValue();
+    }
 }
